b1ce06a719cfcd64530a30442304e0d480e2e36f
Frank Tran <franktranvantu@gmail.com>
2021-02-21 19:11:05 +0700

    Update

--- final/iteration.js
+++ final/iteration.js
@@ -1,14 +1,15 @@
-// Using the filter and map methods on the years array, create an array of display strings for each year in the 21st century (remember: the 21st century starts with the year "2001").
-// Each display string should be the year followed by "A.D." See the comments below to see the correct result.
-// Store the new array in the variable displayYears.
+const users = [
+  {name: 'Samir', age: 27},
+  {name: 'Angela', age: 33},
+  {name: 'Beatrice', age: 42}
+];
 
-const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
-let displayYears;
+// const newUsers = users.filter(user => user.name !== 'Samir');
+const newUsers = users.map(user => `${user.name} is ${user.age} years old.`);
+console.log(newUsers);
 
-// displayYears should be: ["2015 A.D.", "2013 A.D.", "2012 A.D."]
-// Write your code below
-displayYears = years
-  .filter(year => year > 2000)
-  .map(year => `${year} A.D.`);
-
-console.log(displayYears)
+const user = users.reduce((acc, user) => {
+  acc[user.name] = user.age
+  return acc;
+}, {});
+console.log(user);
