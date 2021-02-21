872f3a9a9b4972d0b2fbde404b473162d2e13d82
Frank Tran <franktranvantu@gmail.com>
2021-02-21 19:50:40 +0700

    Update

--- start/iteration.js
+++ start/iteration.js
@@ -1,10 +1,35 @@
-const products = [
-  { name: 'hard drive', price: 59.99 },
-  { name: 'lighbulbs', price: 2.59 },
-  { name: 'paper towels', price: 6.99 },
-  { name: 'flatscreen monitor', price: 159.99 },
-  { name: 'cable ties', price: 19.99 },
-  { name: 'ballpoint pens', price: 4.49 }
-];
+// Using the filter and reduce methods on the purchaseItems array, add the total price of all the groceries (items with a dept. of groceries).
+// Store the total price in the groceryTotal variable.
+// The correct amount can be seen in the comments below.
 
-// Result: { name: 'paper towels', price: 6.99 }
+const purchaseItems = [
+  {
+    name: 'apples',
+    dept: 'groceries',
+    price: 2.49
+  },
+  {
+    name: 'bread',
+    dept: 'groceries',
+    price: 2.99
+  },
+  {
+    name: 'batteries',
+    dept: 'electronics',
+    price: 5.80
+  },
+  {
+    name: 'eggs',
+    dept: 'groceries',
+    price: 3.99
+  },
+  {
+    name: 't-shirts',
+    dept: 'apparel',
+    price: 9.99
+  }
+];
+let groceryTotal;
+
+// groceryTotal should be: 9.47
+// Write your code below
