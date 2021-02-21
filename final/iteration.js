f4a99324a422603cf6e579b0afce3064e4c6eb98
Frank Tran <franktranvantu@gmail.com>
2021-02-21 19:03:02 +0700

    Update

--- final/iteration.js
+++ final/iteration.js
@@ -1,7 +1,14 @@
-const arr = [1,2,3];
+// Using the filter and map methods on the years array, create an array of display strings for each year in the 21st century (remember: the 21st century starts with the year "2001").
+// Each display string should be the year followed by "A.D." See the comments below to see the correct result.
+// Store the new array in the variable displayYears.
 
-const incrementedArr = arr
-  .filter(number => number !== 2)
-  .map(number => number + 1);
+const years = [1989, 2015, 2000, 1999, 2013, 1973, 2012];
+let displayYears;
 
-console.log(incrementedArr); // => [2,4]
+// displayYears should be: ["2015 A.D.", "2013 A.D.", "2012 A.D."]
+// Write your code below
+displayYears = years
+  .filter(year => year > 2000)
+  .map(year => `${year} A.D.`);
+
+console.log(displayYears)
