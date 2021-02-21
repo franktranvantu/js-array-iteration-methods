0264b96e536fbae2ce0f65cfc55b1bd2b7cb1192
Frank Tran <franktranvantu@gmail.com>
2021-02-21 18:55:21 +0700

    Update

--- final/iteration.js
+++ final/iteration.js
@@ -1,5 +1,16 @@
-const prices = [6.75, 3.10, 4.00, 8.12]; // Total: 21.97
+// phoneNumbers is array of 10 digit phone numbers, where the first three digits, in parentheses, are area codes.
+// Using reduce, return the total phone numbers with a 503 area code.
+// Store the total in the variable numberOf503.
 
-let total = prices.reduce((acc, cur) => acc + cur, 0);
+const phoneNumbers = ["(503) 123-4567", "(646) 123-4567", "(503) 987-6543", "(503) 234-5678", "(212) 123-4567", "(416) 123-4567"];
+let numberOf503;
 
-console.log(total);
+// numberOf503 should be: 3
+// Write your code below
+numberOf503 = phoneNumbers.reduce((count, phoneNumber) => {
+  if (phoneNumber.substring(1, 4) === '503') {
+    return count + 1;
+  }
+  return count;
+}, 0);
+console.log(numberOf503);
