eced9169693906db5958a6dd327e5c23496eda52
Frank Tran <franktranvantu@gmail.com>
2021-02-21 19:14:20 +0700

    Update

--- final/iteration.js
+++ final/iteration.js
@@ -1,15 +1,17 @@
-const users = [
-  {name: 'Samir', age: 27},
-  {name: 'Angela', age: 33},
-  {name: 'Beatrice', age: 42}
+// Using the map method on the authors array, create an array of full name strings, comprising the first name, then a space, then the last name.
+// See the comments below for reference.
+// Store the new array in the fullAuthorNames variable.
+
+const authors = [
+  { firstName: "Beatrix", lastName: "Potter" },
+  { firstName: "Ann", lastName: "Martin" },
+  { firstName: "Beverly", lastName: "Cleary" },
+  { firstName: "Roald", lastName: "Dahl" },
+  { firstName: "Lewis", lastName: "Carroll" }
 ];
+let fullAuthorNames;
 
-// const newUsers = users.filter(user => user.name !== 'Samir');
-const newUsers = users.map(user => `${user.name} is ${user.age} years old.`);
-console.log(newUsers);
-
-const user = users.reduce((acc, user) => {
-  acc[user.name] = user.age
-  return acc;
-}, {});
-console.log(user);
+// fullAuthorNames should be: ["Beatrix Potter", "Ann Martin", "Beverly Cleary", "Roald Dahl", "Lewis Carroll"]
+// Write your code below
+fullAuthorNames = authors.map(author => `${author.firstName} ${author.lastName}`);
+console.log(authors);
--- start/iteration.js
+++ start/iteration.js
@@ -1,5 +1,15 @@
-const users = [
-  {name: 'Samir'},
-  {name: 'Angela'},
-  {name: 'Beatrice'}
+// Using the map method on the authors array, create an array of full name strings, comprising the first name, then a space, then the last name.
+// See the comments below for reference.
+// Store the new array in the fullAuthorNames variable.
+
+const authors = [
+  { firstName: "Beatrix", lastName: "Potter" },
+  { firstName: "Ann", lastName: "Martin" },
+  { firstName: "Beverly", lastName: "Cleary" },
+  { firstName: "Roald", lastName: "Dahl" },
+  { firstName: "Lewis", lastName: "Carroll" }
 ];
+let fullAuthorNames;
+
+// fullAuthorNames should be: ["Beatrix Potter", "Ann Martin", "Beverly Cleary", "Roald Dahl", "Lewis Carroll"]
+// Write your code below
